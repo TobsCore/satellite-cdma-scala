b112215f81342a9b04472702e4471b59cf5e7a1f
Tobias Kerst <tobias.kerst@tobscore.com>
2017-01-17 22:05:40 +0100

    Improves code style and performance gravely

--- src/main/scala/hska/embedded/Application.scala
+++ src/main/scala/hska/embedded/Application.scala
@@ -6,14 +6,14 @@ import scala.io.Source
 
 object Application extends LazyLogging {
 
-  val max = 1023 - 3 * 65
-  val min = -1023 + 3 * 65
+  val max: Int = 1023 - 3 * 65
+  val min: Int = -1023 + 3 * 65
 
   def decodeInformation(chipSequence: List[Int], delta: Int, sumSignalEntryList: Array[Int], satelliteID: Int): Option[DecodedInformation] = {
     var scalarProduct = 0
     for (bitPosition <- chipSequence.indices) {
-      val position = (bitPosition + delta) % chipSequence.length
-      scalarProduct += chipSequence(position) * sumSignalEntryList(bitPosition)
+      val position = bitPosition + delta
+      scalarProduct += chipSequence(bitPosition) * sumSignalEntryList(position)
     }
 
     if (scalarProduct < max && scalarProduct > min) {
@@ -31,26 +31,24 @@ object Application extends LazyLogging {
     val resultList = scala.collection.mutable.MutableList[DecodedInformation]()
 
     logger.debug("Generating Chip Sequences")
-    val chipSequencesWithZeros: Array[List[Int]] = ChipSequenceGenerator.generate
+    val chipSequencesWithZeros: Array[List[Int]] = ChipSequenceGenerator.generate()
     val chipSequences: Array[List[Int]] = ChipSequenceGenerator.negateZeros(chipSequencesWithZeros)
 
     logger.debug("Start Decoding Procedure")
-    for ((chipSequence, satelliteID) <- chipSequences.zipWithIndex) {
-      logger.debug(s"Decoding Satellite ${satelliteID}")
-      for (delta <- 0 until 1023) {
+    chipSequences.zipWithIndex.foreach { case (chipSequence, satelliteID) =>
+      logger.debug(s"Decoding Satellite $satelliteID")
+      (0 until 1023).foreach { delta =>
         val decodedInformation: Option[DecodedInformation] = decodeInformation(chipSequence, delta, entries, satelliteID)
 
-        if (decodedInformation.isDefined) {
-          println(decodedInformation.get)
-        }
+        if (decodedInformation.isDefined) println(decodedInformation.get)
       }
     }
     resultList.toList
   }
 
   def dupliateEntriesMethod(list: Array[Int]): Array[Int] = {
-    val resultArray: Array[Int] = new Array[Int](list.length * 2);
-    for((entry, index) <- list.zipWithIndex) {
+    val resultArray: Array[Int] = new Array[Int](list.length * 2)
+    for((_, index) <- list.zipWithIndex) {
       resultArray(index) = list(index)
       resultArray(index + list.length) = list(index)
     }
@@ -65,11 +63,13 @@ object Application extends LazyLogging {
       val filename: String = args(0)
       try {
         val fileContents = Source.fromFile(filename).getLines.mkString
+        logger.info(s"Reading file $filename")
         val entries: Array[Int] = fileContents.split(" ").map(_.toInt)
         val entriesDup: Array[Int] = dupliateEntriesMethod(entries)
         
 
-        val resultSet: Seq[DecodedInformation] = decode(entries);
+        decode(entriesDup)
+        //val resultSet: Seq[DecodedInformation] = decode(entriesDup);
         //resultSet.foreach(println)
       } catch {
         case ex: FileNotFoundException => logger.error(s"File $filename cannot be found")
